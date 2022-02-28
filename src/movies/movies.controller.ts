5e6872528606c2973a4f1dec9db8514af48f1b66
Seung Bum <wer06099@naver.com>
2022-02-28 14:57:14 +0900

    Service Part two

---
+++ src/movies/movies.controller.ts
@@ -0,0 +1,47 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
+import { Movie } from './entities/movie.entity';
+import { MoviesService } from './movies.service';
+
+@Controller('movies') //router 경로
+export class MoviesController {
+  constructor(private readonly moviesService: MoviesService) {}
+
+  @Get()
+  getAll(): Movie[] {
+    return this.moviesService.getAll();
+  }
+
+  @Get('search')
+  search(@Query('title') searchTitle: string) {
+    return `What is this a ${searchTitle}`;
+  }
+
+  @Get(':id')
+  getOne(@Param('id') id: Number): Movie {
+    return this.moviesService.getOne(id);
+  }
+
+  @Post()
+  create(@Body() movieData) {
+    return this.moviesService.create(movieData);
+  }
+
+  @Delete(':id')
+  delete(@Param('id') id: Number) {
+    return this.moviesService.deleteOne(id);
+  }
+
+  @Patch(':id')
+  patch(@Param('id') id: Number, @Body() updateData) {
+    return this.moviesService.update(id, updateData);
+  }
+}
