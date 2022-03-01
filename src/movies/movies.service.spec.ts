6272f8202806b8eac7ee253c4e38f6be3d51db27
Seung Bum <wer06099@naver.com>
2022-03-01 13:35:54 +0900

    UnitTest Update and Cov Check

--- src/movies/movies.service.spec.ts
+++ src/movies/movies.service.spec.ts
@@ -73,11 +73,34 @@ describe('MoviesService', () => {
 
   describe('create', () => {
     it('should create a movie', () => {
+      const beforeCreate = service.getAll().length;
       service.create({
         title: 'Test',
         geners: ['test'],
         year: 2000,
       });
+      const afterCreate = service.getAll().length;
+      expect(afterCreate).toBeGreaterThan(beforeCreate);
+    });
+  });
+
+  describe('update', () => {
+    it('should update a movie', () => {
+      service.create({
+        title: 'Test',
+        geners: ['test'],
+        year: 2000,
+      });
+      service.update(1, { title: 'Update Test' });
+      const movie = service.getOne(1);
+      expect(movie.title).toEqual('Update Test');
+    });
+    it('should throw a NotFoundException', () => {
+      try {
+        service.update(999, {});
+      } catch (e) {
+        expect(e).toBeInstanceOf(NotFoundException);
+      }
     });
   });
 });
